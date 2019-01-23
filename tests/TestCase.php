e7ae4e324033d140752fe9b009f72e3581ba4356
zerofairnight <zerofairnight@gmail.com>
2019-01-23 12:46:12 +0100

    add some basic testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace FileConverter\Tests;
 
+use FileConverter\FileConverter;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Create a new file converter instance from the specified file.
+     *
+     * @param mixed $file
+     * @return \FileConverter\FileConverter
+     */
+    protected function convert($file)
+    {
+        return new FileConverter($file);
+    }
 }
--- tests/Unit/FileConverterTest.php
+++ tests/Unit/FileConverterTest.php
@@ -6,5 +6,13 @@ use FileConverter\Tests\TestCase;
 
 class FileConverterTest extends TestCase
 {
-    //
+    public function test_detect_webm_mime()
+    {
+        $filename = __DIR__.'/../files/big-buck-bunny_trailer.webm';
+
+        $convert = $this->convert($filename);
+
+        $this->assertEquals($convert->mimeType(), 'video/webm');
+        $this->assertContains('video/mp4', $convert->availableFormats());
+    }
 }
